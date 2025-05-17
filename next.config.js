182a3a1eeb664ed64ec6233ba95f44e89829d77d
Oliver Fryatt <oliverfryatt@Olivers-MacBook-Air.local>
2025-05-16 21:09:02 -0400

    chore: disable ESLint and TypeScript build errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
       // Disable ESLint checks during production builds to avoid build failures
       ignoreDuringBuilds: true,
     },
+    typescript: {
+      // Disable type-checking errors during production builds
+      ignoreBuildErrors: true,
+    },
   };
   
   module.exports = nextConfig;
